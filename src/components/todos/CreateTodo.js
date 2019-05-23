c0d5a0e9cb3d2eb78f7ab7fea4ee026fd5ccd40d
Laura Bodell <bodelllaura@gmail.com>
2019-05-23 00:02:44 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react'
 
 class CreateTodo extends Component {
   
-  
+  constructor() {
+  super();
+  this.state = {
+    text: '',
+  };
+}
+ 
+handleChange(event) {
+  this.setState({
+    text: event.target.value
+  });
+};
   
   
   
